a23c6bd91dedcf23d4053da52151665038a11df0
Maryo Botros <mbotros1912@gmail.com>
2023-09-19 18:34:16 -0700

    Added all services to the Services page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Services from './components/Services/Services';
+import ThreeDPrintingService from './components/ThreeDPrintingService/ThreeDPrintingService';
 
 function App() {
   return (
@@ -17,8 +18,12 @@ function App() {
             <Home />
           </Route>
           {/* Route for Services page */}
-          <Route path="/services">
+          <Route exact path="/services">
             <Services />
+          </Route>
+          {/* Routes for individual service pages */}
+          <Route path="/services/3d-printing">
+            <ThreeDPrintingService />
           </Route>
         </Switch>
       </div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -4,15 +4,45 @@ import './Services.css';
 function Services(){
     return(
         <div className="services">
-            <h1>Services Offered</h1>
-            <ul>
-                <li>3D Printing</li>
-                <li>Sewing</li>
-                <li>Audio Both</li>
-                <li>Button Making</li>
-                <li>Sticker Printing</li>
-                <li>iMacs with Adobe Creative Cloud</li>
-            </ul>
+            <h1>Services</h1>
+            <div className="service-grid">
+                {/* 3D Printing */}
+                <Link to="/services/3d-printing">
+                    <div className="service-box">
+                        <p>3D printing</p>
+                    </div>
+                </Link>
+                {/* Sewing */}
+                <Link to="/services/sewing">
+                    <div className="service-box">
+                        <p>Sewing</p>
+                    </div>
+                </Link>
+                {/* Audio Booth */}
+                <Link to="/services/audio-booth">
+                    <div className="service-box">
+                        <p>Audio Booth</p>
+                    </div>
+                </Link>
+                {/* Button Making */}
+                <Link to="/services/button-making">
+                    <div className="service-box">
+                        <p>Button Making</p>
+                    </div>
+                </Link>
+                {/* Sticker Printing */}
+                <Link to="/services/sticker-printing">
+                    <div className="service-box">
+                        <p>Sticker Printing</p>
+                    </div>
+                </Link>
+                {/* iMacs with Adobe Creative Cloud */}
+                <Link to="/services/imacs-adobe">
+                    <div className="service-box">
+                        <p>iMacs with Adobe Creative Cloud</p>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
